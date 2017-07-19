41bd87b5a417bb943b90f3ca61168680567d3c5a
Akshit <akshitagg51995@gmail.com>
2017-07-19 13:04:41 +0530

    Second commit

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+import datetime
+
+def signup_view(request):
+    today = datetime.now()
+    return render(request, 'index.html', {'today': today})
